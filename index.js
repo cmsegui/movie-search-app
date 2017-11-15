2ca23553759f081657d9ddd5670e07df1bf3cc29
cydmarie <cydmarie.dev@gmail.com>
2017-11-15 23:19:43 +0000

    parsed json data and played around with getting different results

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ var request = require("request");
 app.get("/results", function(req, res) {
     request("http://omdbapi.com/?s=california&apikey=thewdb", function(error, response, body) {
         if(!error && response.statusCode === 200) {
-            var parsedData = JSON.parse(body);
-            res.send(body);
+            var results = JSON.parse(body);
+            res.send(results["Search"][0]);
         }
     });
 });
